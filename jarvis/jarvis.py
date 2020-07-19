13b93985f797f2f374874ab40336d286ab101afb
Nischay <iamnischay241@gmail.com>
2020-07-19 19:06:47 +0530

    my assistant

---
+++ jarvis/jarvis.py
@@ -0,0 +1,88 @@
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition
+import datetime
+import wikipedia #pip install wikipedia
+import webbrowser
+import os
+import smtplib
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[1].id)
+engine.setProperty('voice', voices[1].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")
+
+    else:
+        speak("Good Evening!")
+
+    speak("I am ,your.assistant. Please tell me how may I help you")
+
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)
+        print("Say that again please...")
+        return "None"
+    return query
+wishMe()
+query = takeCommand()
+#logic for basic task
+if 'wikipedia' in query.lower():
+    speak('Searching wikipedia...')
+    query = query.replace("wikipedia","")
+    results = wikipedia.summary(query,sentences=2)
+    print(results)
+    speak(results)
+elif 'open youtube' in query.lower():
+    #webbrowser.open("youtube.com ")
+    url = "youtube.com"
+    chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+    webbrowser.get(chrome_path).open(url)
+elif 'open google' in query.lower():
+    # webbrowser.open("youtube.com ")
+    url = "google.com"
+    chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+    webbrowser.get(chrome_path).open(url)
+elif 'open reddit' in query.lower():
+    # webbrowser.open("youtube.com ")
+    url = "reddit.com"
+    chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+    webbrowser.get(chrome_path).open(url)
+elif 'play songs' in query.lower():
+    songs_dir ="C:\\Users\\LENOVO\\Music"
+    songs = os.listdir(songs_dir)
+    print(songs)
+    os.startfile(os.path.join(songs_dir,songs[0]))
+elif 'time' in query.lower():
+    strTime = datetime.datetime.now().strftime("%H:%M:%S:")
+    print(strTime)
+    speak("The time is:" + strTime)
+
+elif 'open code' in query:
+    codePath = "C:\\Users\\Haris\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+    os.startfile(codePath)
